1e542061010ef0e6ea109281df656524d1fa219b
Harshit-3905 <harshitjoshileet@gmail.com>
2023-07-17 11:27:27 +0530

    Solution File Updated

--- Solution.java
+++ Solution.java
@@ -1,16 +1,47 @@
 import java.util.*;
 import java.io.*;
 
-class Solution {
+public class Solution {
     public static void main(String[] args) throws java.lang.Exception {
         FastReader in = new FastReader();
-        int testcase = in.nextInt();
+        int testcase = 1;
         while (testcase-- > 0) {
             int n = in.nextInt();
 
         }
     }
 
+    // For Integer Array
+    public static void safeSort(int[] a) {
+        shuffle(a);
+        Arrays.sort(a);
+    }
+
+    public static void shuffle(int[] a) {
+        Random get = new Random();
+        for (int i = 0; i < a.length; i++) {
+            int r = get.nextInt(a.length);
+            int temp = a[i];
+            a[i] = a[r];
+            a[r] = temp;
+        }
+    }
+
+    // For Long Array
+    public static void safeSortl(long[] a) {
+        shufflel(a);
+        Arrays.sort(a);
+    }
+
+    public static void shufflel(long[] a) {
+        Random get = new Random();
+        for (int i = 0; i < a.length; i++) {
+            int r = get.nextInt(a.length);
+            long temp = a[i];
+            a[i] = a[r];
+            a[r] = temp;
+        }
+    }
 }
 
 class FastReader {
